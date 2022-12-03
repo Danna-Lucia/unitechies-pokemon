d7fb4b30d2710db0ea3c59675c4d1860fe6726ea
tanya110900 <115852502+tanya110900@users.noreply.github.com>
2022-12-03 03:48:36 +0000

    puse propiedades en la clase character

---
+++ Character.py
@@ -0,0 +1,7 @@
+class Character(): //Se crea la clase Character
+
+    name ="" //Propiedad nombre
+    health_points = 0 //Propiedad puntos de vida se inicializa en 0
+    speed = 0 // Propiedad rapidez se inicializa en 0
+    defense = 0 //Propiedad defensa se inicializa en 0
+    moves = "" //Propiedad movimientos
