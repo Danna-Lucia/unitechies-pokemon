72d162c743bf7369cb95f1cfa5445629b30cf35b
keving36 <115852462+keving36@users.noreply.github.com>
2022-12-03 03:44:00 +0000

    cree la clase battle

---
+++ battle.py
@@ -0,0 +1,14 @@
+class Battle(): 
+    player = "Player"
+    bot = "Bot"
+    def player(self):
+        pass
+        def introduction(self): 
+            pass
+            def run(self):
+                pass
+                def show_winner(self):
+                    pass
+                    def who_goes_first(player:Player, bot:Bot):
+                        pass
+                        
